5db02f221df9620447c001a2a496ae4bea58b196
Tanguy VANSNICK <191451@umons.ac.be>
2024-10-26 15:37:32 +0200

    createcontent

--- frontend/app/create/CreateContent.tsx
+++ frontend/app/create/CreateContent.tsx
@@ -1,4 +1,17 @@
 "use client";
+
+declare global {
+    interface Window {
+      Telegram?: {
+        WebApp: {
+          initDataUnsafe: {
+            start_param: string;
+          };
+        };
+      };
+    }
+  }
+
 import { useEffect, useState } from "react";
 import {
   DynamicWidget,
@@ -7,15 +20,14 @@ import {
 } from "../../lib/dynamic";
 import Spinner from "../Spinner";
 import Create from "../components/Create";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function CreateContent() {
   const { sdkHasLoaded, user } = useDynamicContext();
   const { telegramSignIn } = useTelegramLogin();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const router = useRouter();
-  const searchParams = useSearchParams();
-  const [chatId, setChatId] = useState<string | null>(null);
+  const [groupId, setGroupId] = useState<string | null>(null);
 
   useEffect(() => {
     if (!sdkHasLoaded) return;
@@ -29,20 +41,32 @@ export default function CreateContent() {
 
     signIn();
 
-    // Récupérer le chatId des paramètres de l'URL
-    const chatIdFromUrl = searchParams.get('chatId');
-    setChatId(chatIdFromUrl);
-  }, [sdkHasLoaded, telegramSignIn, user, searchParams]);
+    // Récupérer et décoder le groupId du paramètre de démarrage Telegram
+    if (window.Telegram?.WebApp) {
+      const startParam = window.Telegram.WebApp.initDataUnsafe.start_param;
+      if (startParam) {
+        try {
+          const decodedGroupId = atob(startParam);
+          console.log("Decoded Group ID:", decodedGroupId);
+          setGroupId(decodedGroupId);
+        } catch (error) {
+          console.error("Error decoding group ID:", error);
+        }
+      } else {
+        console.log("No start_param available");
+      }
+    }
+  }, [sdkHasLoaded, telegramSignIn, user]);
 
   return (
     <>
       {isLoading ? (
         <Spinner />
-      ) : (
+      ) : ( 
         <>
-          <Create onBack={() => router.push("/Home")} />
+          <Create onBack={() => router.push("/Home")}/>
           <div className="mt-4 p-2 bg-gray-800 rounded">
-            Telegram Group ID: {chatId ? chatId : "No telegram id"}
+            Telegram Group ID: {groupId ? groupId : "No group ID available"}
           </div>
         </>
       )}
